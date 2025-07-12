048fa6d0c173c9bee330363236712ea1f3729478
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-07-12 20:24:38 +0600

    loop

--- basic python/User Input and While Loops.py
+++ basic python/User Input and While Loops.py
@@ -13,4 +13,26 @@ if height >= 36:
     print("\nYou're tall enough to ride!")
 else:
     print("\nYou'll be able to ride when you're a little older!")
-    
+
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+if int(number) % 2==0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")#  a string.
+#  The `input()` function can also take a prompt string to display to the user.     
+
+#While loop structure
+#  A while loop continues to execute as long as a condition is true.
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1  # Increment the number by 1 each time through the loop 
+
+promt = "\nTell me something, and I will repeat it back to you."
+promt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(promt)
+    if message != 'quit':
+        print(message)
