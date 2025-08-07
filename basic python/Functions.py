5cc683b11b3a41db79766f89ed2bc63ecc166c75
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-07 22:45:24 +0600

    f2

--- basic python/Functions.py
+++ basic python/Functions.py
@@ -6,3 +6,10 @@ def greet_user(username):
     print(f"Hello, welcome to the Python programming world!, {username.title()}!")    
 
 greet_user("Alice,habibi")
+
+#Passing arguments
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
