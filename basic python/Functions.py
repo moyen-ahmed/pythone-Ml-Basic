8d1e48c2c8534bfc13afb603d47ccf09e5ebd981
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-07 22:40:48 +0600

    wer

---
+++ basic python/Functions.py
@@ -0,0 +1,8 @@
+# def greet_user(username):
+#     print(f"Hello, welcome to the Python programming world!,{username.tittle()}!")    
+# greet_user("Alice,habibi")
+
+def greet_user(username):
+    print(f"Hello, welcome to the Python programming world!, {username.title()}!")    
+
+greet_user("Alice,habibi")
