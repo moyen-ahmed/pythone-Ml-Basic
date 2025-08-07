a9b495005f4a438776022eb6f64e8ab5014adbaa
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-08 01:44:24 +0600

    dfgb

--- basic python/Functions.py
+++ basic python/Functions.py
@@ -80,3 +80,8 @@
 import pizza
 pizza.make_pizza(16, 'pepperoni')
 pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+from pizza import make_pizza
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# #Importing an entire module
