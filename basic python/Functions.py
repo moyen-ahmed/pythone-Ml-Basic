4adc12b65a0134b8d679a70de4f51b02288fbeb7
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-07 22:57:34 +0600

    af

--- basic python/Functions.py
+++ basic python/Functions.py
@@ -13,3 +13,10 @@ def describe_pet(animal_type, pet_name):
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 describe_pet('hamster', 'harry')
 describe_pet('dog', 'willie')
+
+#Returning a value
+def get_formatted_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
