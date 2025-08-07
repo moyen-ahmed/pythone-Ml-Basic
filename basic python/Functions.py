1e1338ceac59a84a78273c637e08c4e292e91f0a
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-08 01:45:38 +0600

    asfd

--- basic python/Functions.py
+++ basic python/Functions.py
@@ -84,4 +84,8 @@ pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 from pizza import make_pizza
 make_pizza(16, 'pepperoni')
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
-# #Importing an entire module
+ #Importing an entire module
+from pizza import make_pizza  as mp
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+# #Importing an entire module    
