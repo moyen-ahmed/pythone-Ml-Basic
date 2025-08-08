82d04989dfd95875bfaeea78da1f58c598b1e8e7
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-08 21:26:11 +0600

    Fix typos and improve output formatting in OOP_and_class.py

--- basic python/OOP_and_class.py
+++ basic python/OOP_and_class.py
@@ -6,4 +6,16 @@ class Dog:
         self.age=age
         
 def sit(self):
-    print(f"{self.naem}is now s")
+    print(f"{self.name}is now sitting")
+def roll_over(self):
+    print(f"{self.name}rolled over!")
+    
+my_dog =Dog('Willy',6)
+your_dog=Dog('Lucy',3)
+
+print(f"My dog name is {my_dog.name}.")
+print(f"My dog is{your_dog.age} years old.")
+my_dog.sit()
+print(f"\Your dog's name is {your_dog.name}")
+print(f"Your dog is {your_dog.age}years old.")
+your_dog.sit()
