e9fecc9b44aa4fa9e1a445c242ade9f175a91c6e
Ishtiak Ahmed Moyen <estyakahmefmoyen@gmail.com>
2025-08-09 14:41:37 +0600

    erer

--- basic python/opppractice/user.py
+++ basic python/opppractice/user.py
@@ -1,2 +1,18 @@
 class Resturent:
+    def __init__(self,restaurent_name,cuisine_name):
+        self.restaturent_name=restaurent_name
+        self.cuisine_type=cuisine_name
+def describe_resturent(self):
+    print(f"The restuernt is called{self.resturet_name}")
+    print(f"The cuisine type is{self.cuisine_type}.")
+def open_resturant(self):
+    print(f"The resturent{self.resturet_name}")      
+
+resturent=Resturent('KFC','Bangladesh')
+print(f"Resturent name:{resturent.restaurent_name}")
+print(f"Resturent cusine :{resturent.cuisine_type}")
+
+def open_resturantdef(self):
+    print(f"The resturent {self.restaurent_name}")
     
+resturent=     
